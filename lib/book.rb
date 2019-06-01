5112eb70e70d6457d80994405ae5c47b7bcd3572
Silvia Mendoza <silviamendoza223@yahoo.com>
2019-06-01 22:55:15 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -20,6 +20,16 @@ class Book
         @page_count
       end
       
+      def genre=(genre)
+        @genre=genre
+      end
+      
+      def genre
+        @genre
+      end
+      
+      
+      
       
         
         
